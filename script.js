0d1eabddc99cff6988b5035d4eb03a9bb0e7b446
Michellas93 <100272452+Michellas93@users.noreply.github.com>
2023-02-18 14:00:54 +0100

    cela slozka

---
+++ script.js
@@ -0,0 +1,43 @@
+let secret = Math.trunc(Math.random() * 20) + 1;
+let score = 13;
+let highscore = 0;
+const display = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    display("🛑 Žádné číslo!");
+  } else if (guess === secret) {
+    display("Číslo je správně🥳");
+    document.querySelector(".number").textContent = secret;
+    document.querySelector(".number").style.width = "40 rem";
+    document.querySelector("body").style.backgroundColor = "#BD4F6C";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess !== secret) {
+    if (score > 1) {
+      display(guess > secret ? "Příliš velké číslo" : "Příliš nízké číslo");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      display("Prohráváš");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".btn").addEventListener("click", function () {
+  score = 20;
+  secret = Math.trunc(Math.random() * 20) + 1;
+  display("Začni hádat");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".number").style.width = "15 rem";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#EEC0C6";
+});
