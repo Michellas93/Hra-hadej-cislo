33423b05acececfe72ae8f077e9d0e1143fc4682
Michellas93 <100272452+Michellas93@users.noreply.github.com>
2023-08-20 20:06:54 +0200

    change css

--- script.js
+++ script.js
@@ -2,43 +2,43 @@ let secret = Math.trunc(Math.random() * 20) + 1;
 let score = 13;
 let highscore = 0;
 const display = function (message) {
-  document.querySelector(".message").textContent = message;
+	document.querySelector(".message").textContent = message;
 };
 
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
+	const guess = Number(document.querySelector(".guess").value);
 
-  if (!guess) {
-    display("🛑 Žádné číslo!");
-  } else if (guess === secret) {
-    display("Číslo je správně🥳");
-    document.querySelector(".message").style.color = "#8D918D";
-    document.querySelector(".number").textContent = secret;
-    document.querySelector(".number").style.width = "40 rem";
-    document.querySelector("body").style.backgroundColor = "#B6B6B4";
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector(".highscore").textContent = highscore;
-    }
-  } else if (guess !== secret) {
-    if (score > 1) {
-      display(guess > secret ? "Příliš velké číslo" : "Příliš nízké číslo");
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      display("Prohráváš");
-      document.querySelector(".score").textContent = 0;
-    }
-  }
+	if (!guess) {
+		display("🛑 Žádné číslo!");
+	} else if (guess === secret) {
+		display("Číslo je správně🥳");
+		document.querySelector(".message").style.color = "#ff00bf";
+		document.querySelector(".number").textContent = secret;
+		document.querySelector(".number").style.width = "40 rem";
+		document.querySelector("body").style.backgroundColor = "#ffcbcb";
+		if (score > highscore) {
+			highscore = score;
+			document.querySelector(".highscore").textContent = highscore;
+		}
+	} else if (guess !== secret) {
+		if (score > 1) {
+			display(guess > secret ? "Příliš velké číslo" : "Příliš nízké číslo");
+			score--;
+			document.querySelector(".score").textContent = score;
+		} else {
+			display("Prohráváš");
+			document.querySelector(".score").textContent = 0;
+		}
+	}
 });
 
 document.querySelector(".btn").addEventListener("click", function () {
-  score = 20;
-  secret = Math.trunc(Math.random() * 20) + 1;
-  display("Začni hádat");
-  document.querySelector(".score").textContent = score;
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".number").style.width = "17 rem";
-  document.querySelector(".guess").value = "";
-  document.querySelector("body").style.backgroundColor = "#25383C";
+	score = 20;
+	secret = Math.trunc(Math.random() * 20) + 1;
+	display("Začni hádat");
+	document.querySelector(".score").textContent = score;
+	document.querySelector(".number").textContent = "?";
+	document.querySelector(".number").style.width = "17 rem";
+	document.querySelector(".guess").value = "";
+	document.querySelector("body").style.backgroundColor = "#FFC0CB";
 });
